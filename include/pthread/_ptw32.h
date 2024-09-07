1406665b0ee72ba52b3736cf6e677556e024d96d
tokenscreator <conngunay3@gmail.com>
2024-09-07 13:44:30 +0700

    fix threadwin32 build android
    chỉnh sửa lại để build được cả win32 và android

--- include/pthread/_ptw32.h
+++ include/pthread/_ptw32.h
@@ -19,17 +19,17 @@
  *      code distribution. The list can also be seen at the
  *      following World Wide Web location:
  *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
+ *
  *      This library is free software; you can redistribute it and/or
  *      modify it under the terms of the GNU Lesser General Public
  *      License as published by the Free Software Foundation; either
  *      version 2 of the License, or (at your option) any later version.
- * 
+ *
  *      This library is distributed in the hope that it will be useful,
  *      but WITHOUT ANY WARRANTY; without even the implied warranty of
  *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *      Lesser General Public License for more details.
- * 
+ *
  *      You should have received a copy of the GNU Lesser General Public
  *      License along with this library in the file COPYING.LIB;
  *      if not, write to the Free Software Foundation, Inc.,
@@ -51,7 +51,7 @@
 #define  PTW32_VERSION 3,0,3,1
 #define  PTW32_VERSION_STRING "3, 0, 3, 1\0"
 
-// skip the rest when running this through the Microsoft Resource Compiler, which is a VERY brittle piece of machinery! -> RC2188 & RC1116 cryptic failures will be your part! 
+// skip the rest when running this through the Microsoft Resource Compiler, which is a VERY brittle piece of machinery! -> RC2188 & RC1116 cryptic failures will be your part!
 #ifndef RC_INVOKED
 
 #if defined(__GNUC__)
